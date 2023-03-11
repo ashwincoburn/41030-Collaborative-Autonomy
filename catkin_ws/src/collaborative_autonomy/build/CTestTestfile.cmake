462ab1512b8bbb04b3c0ee75af701d5345ac81c4
ashwincoburn <ashwincoburn@gmail.com>
2023-03-12 09:36:51 +1100

    Create Package & Notes on ROS

---
+++ catkin_ws/src/collaborative_autonomy/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/arcbash/catkin_ws/src/collaborative_autonomy
+# Build directory: /home/arcbash/catkin_ws/src/collaborative_autonomy/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
