c8757e7d049382695e1fef20feca849beba9984f
ashwincoburn <ashwincoburn@gmail.com>
2023-05-18 13:51:29 +1000

    update

--- collaborative_autonomy/src/senario2/chaser_brain.cpp
+++ collaborative_autonomy/src/senario2/chaser_brain.cpp
@@ -11,16 +11,26 @@ using namespace std;
 
 /* This is designed to chase the LEADER TB3 */
 /* Simple chase algorithm that stops chasing if within proximity of LEADER */
+/* As we have a subscriber, looks like we'll need ros::spin() */
+// http://wiki.ros.org/evarobot_odometry/Tutorials/indigo/Writing%20a%20Simple%20Subscriber%20for%20Odometry
+// <node name="chaser_brain" pkg="collaborative_autonomy"  type="chaser_brain" output="screen" launch-prefix="gnome-terminal --command"/>
+
+void chaserCallback(){
+
+}
+
 
 int main(int argc, char** argv){
 
   ros::init(argc, argv, "chaser_brain");
   ROS_INFO_STREAM("STARTING chaser_brain...");
+  ros::NodeHandle nh;
 
-  while(ros::ok()){
-    ROS_INFO_STREAM("Chaser Placeholder running...");
-    this_thread::sleep_for (chrono::milliseconds(2000));
-  }
+  ros::Subscriber sub = n.subscribe("tb3/leader/odom", 1000, chatterCallback);
+
+  ros::spin();
+
+  ros::shutdown();
 
   return 0;
 }
